51972ffb292d667f92c0844cf35ee8a40ed108b0
David da Silva <62728470+DaviSilva70@users.noreply.github.com>
2020-04-01 19:28:43 -0300

    Add files via upload

---
+++ Exercicio_14.py
@@ -0,0 +1,5 @@
+
+# Escreva um Programa que converta um aTemperatura digitada em 'C convertida em F'.
+C = float(input('Informe a Temperatura em Cª:'))
+F = ((9*C)/5) + 32
+print('A Temepratura de {0}C CORRESPONDE A {1}F'.format(C,F))

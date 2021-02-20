ada9eb1dfc3df6a78e10dec190bdd5a1534c3e3d
David da Silva <62728470+DaviSilva70@users.noreply.github.com>
2021-02-20 20:39:10 -0300

    Add files via upload

---
+++ ping_simples..py
@@ -0,0 +1,10 @@
+# Importadndo a Biblioteca recursos do Sistema Opracional.
+import os
+# Disparador de Ping Simples.
+print('#'*40) # Imprimindo #*60 vezes.
+ip_ou_host = input(' Digite o IP a ser verification : ') # Foi Criado uma Variavel .
+print('-'*30) # Imprimindo #*60 vezes.
+os.system('ping -n 6 {}'.format(ip_ou_host))# Chamando o Modulo System da Biblioteca os - comando Ping vai numero de Pacotes eo Ip {}.
+print('-'*30) # Imprimindo #*60 vezes.
+
+
